75e105de339d8508e4000b59ea9e255bd4d752de
adrielle <dryka_morais@yahoo.com.br>
2025-01-08 17:10:43 -0300

    feat: add squads and users components with templates and styles, update home component to include them

--- pd-hours-ui/src/app/home/home.component.ts
+++ pd-hours-ui/src/app/home/home.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import {MatTabsModule} from '@angular/material/tabs';
 import { NavbarComponent } from '../shared/navbar/navbar.component';
+import { SquadsComponent } from './squads/squads.component';
+import { UsersComponent } from './users/users.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     NavbarComponent,
+    SquadsComponent,
+    UsersComponent,
     MatTabsModule,
   ],
   templateUrl: './home.component.html',
---
+++ pd-hours-ui/src/app/home/squads/squads.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-squads',
+  imports: [],
+  templateUrl: './squads.component.html',
+  styleUrl: './squads.component.scss'
+})
+export class SquadsComponent {
+addNewSquad() {
+throw new Error('Method not implemented.');
+}
+
+}
